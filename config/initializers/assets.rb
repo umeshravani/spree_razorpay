5dca212e2a46824d6d82250a750a1a9f664e5942
Umesh Ravani <umeshravani98@gmail.com>
2025-12-03 23:25:03 +0530

    Added Support for Spree 5.2+ (Propshaft) as well as older 5.0+ versions (Sprockets) support
    Wrapped asset precompilation in a conditional block (if defined?(Sprockets)). This prevents errors in Spree 5.2+ (Propshaft) while keeping support for Spree 4.x/5.0.
    
    Fix for both Older versions as well as new 5.2 versions to deal with Propshaft and SpRockets without asset errors.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,10 @@
-# Logo Location
-Rails.application.config.assets.precompile += %w[
+# Only run this if Sprockets is loaded (Spree 4.x / 5.0)
+if Rails.configuration.respond_to?(:assets) && defined?(Sprockets)
+  Rails.application.config.assets.precompile += %w[
+  payment_icons/razorpay_logo_dark.svg
+  payment_icons/razorpay_logo_light.svg
   payment_icons/razorpay.svg
   payment_icons/razorpaycheckout.svg
+  payment_icons/window_modal.svg
 ]
+end
