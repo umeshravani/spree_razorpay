8c6d2de84687b8eb001e087e86d8742da06b3275
Umesh Ravani <umeshravani98@gmail.com>
2025-12-22 16:04:50 +0530

    Update install_generator.rb
    Auto-Add Widget to PDP 2.0 product sections, Things this implementation does to install Generator:
    
    1. Find the Product Details Section (PDP)
    2. Check if the block already exists (to prevent duplicates)
    3. Create the block at the end of the section

--- lib/generators/spree_razorpay_checkout/install/install_generator.rb
+++ lib/generators/spree_razorpay_checkout/install/install_generator.rb
@@ -1,12 +1,12 @@
 module SpreeRazorpayCheckout
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('templates', __dir__) # if you use templates
+      source_root File.expand_path('templates', __dir__)
       class_option :migrate, type: :boolean, default: true
 
       def add_javascripts
-        #append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_razorpay_checkout\n"
-        #append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/process_razorpay\n"
+        # append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_razorpay_checkout\n"
+        # append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/process_razorpay\n"
       end
 
       def add_migrations
@@ -19,6 +19,24 @@ module SpreeRazorpayCheckout
         else
           say_status :skip, "Skipped running migrations. You can run them later with `bin/rails db:migrate`.", :yellow
         end
+      end
+
+      def add_razorpay_widget_block
+        say_status :spree_razorpay_checkout, "Adding Razorpay Affordability widget to Product Details", :green
+        require Rails.root.join("config/environment")
+        ::Spree::PageSection
+          .where(type: "Spree::PageSections::ProductDetails")
+          .find_each do |section|
+            next if section.blocks.exists?(
+              type: "Spree::PageBlocks::Products::RazorpayAffordability"
+            )
+            section.blocks.create!(
+              type: "Spree::PageBlocks::Products::RazorpayAffordability",
+              position: section.blocks.maximum(:position).to_i + 1
+            )
+            
+            say_status :created, "Added Razorpay block to section #{section.name}", :green
+          end
       end
     end
   end
