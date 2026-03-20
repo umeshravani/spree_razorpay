532354de99086326377c40117b1574656a5b7e70
Umesh Ravani <umeshravani98@gmail.com>
2026-03-20 20:14:21 +0530

    Update Razorpay routes to include webhooks
    Replaced existing Razorpay routes with a new webhook route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.add_routes do
   
-  post '/razorpay/create_order', to: 'razorpay#create_order'
-  #Accept both POST and PATCH
-  match '/razorpay/response', to: 'razorpay#razor_response', via: [:post, :patch], as: :razorpay_response
+  namespace :razorpay do
+    post :webhooks, to: 'webhooks#create'
+  end
 end
