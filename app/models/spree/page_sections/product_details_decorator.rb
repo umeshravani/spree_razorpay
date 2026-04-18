4d31caa22cdde159cfdf8ada2b8b789813ba1b0a
Umesh Ravani <umeshravani98@gmail.com>
2026-04-19 04:04:35 +0530

    Refactor ProductDetailsDecorator module structure

--- app/models/spree/page_sections/product_details_decorator.rb
+++ app/models/spree/page_sections/product_details_decorator.rb
@@ -1,17 +1,15 @@
 module Spree
-    module PageSections
-      module ProductDetailsDecorator
-        def default_blocks
-          super + [Spree::PageBlocks::Products::RazorpayAffordability.new]
-        end
-  
-        def available_blocks_to_add
-          super + [Spree::PageBlocks::Products::RazorpayAffordability]
-        end
+  module PageSections
+    module ProductDetailsDecorator
+      def default_blocks
+        super + [Spree::PageBlocks::Products::RazorpayAffordability.new]
+      end
+
+      def available_blocks_to_add
+        super + [Spree::PageBlocks::Products::RazorpayAffordability]
       end
     end
   end
-  
-  Spree::PageSections::ProductDetails.prepend(
-    Spree::PageSections::ProductDetailsDecorator
-  )
+end
+
+Spree::PageSections::ProductDetails.prepend(Spree::PageSections::ProductDetailsDecorator) if defined?(Spree::PageSections::ProductDetails)
