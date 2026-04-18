b7c40d73bdb366d5d21700e473d085b5259d5ddf
Umesh Ravani <umeshravani98@gmail.com>
2026-04-19 03:48:45 +0530

    Refactor refund process method for clarity

--- app/models/spree_razorpay_checkout/spree/refund_decorator.rb
+++ app/models/spree_razorpay_checkout/spree/refund_decorator.rb
@@ -2,35 +2,36 @@ module SpreeRazorpayCheckout
   module Spree
     module RefundDecorator
       def process!(credit_cents)
-  response = if payment.payment_method.payment_profiles_supported?
-               payment.payment_method.credit(
-                 credit_cents,
-                 payment.source,
-                 payment.transaction_id,
-                 originator: self
-               )
-             else
-               razorpay_payment_id = payment.source&.razorpay_payment_id || payment.transaction_id
-               payment.payment_method.credit(
-                 credit_cents,
-                 razorpay_payment_id,
-                 originator: self
-               )
-             end
+        response = if payment.payment_method.payment_profiles_supported?
+                     payment.payment_method.credit(
+                       credit_cents,
+                       payment.source,
+                       payment.transaction_id,
+                       originator: self
+                     )
+                   else
+                     razorpay_payment_id = payment.source&.razorpay_payment_id || payment.transaction_id
+                     payment.payment_method.credit(
+                       credit_cents,
+                       razorpay_payment_id,
+                       originator: self
+                     )
+                   end
 
-  unless response.success?
-    Rails.logger.error(Spree.t(:gateway_error) + "  #{response.to_yaml}")
-    text = response.params['message'] || response.params['response_reason_text'] || response.message
-    raise Core::GatewayError, text
-  end
+        unless response.success?
+          Rails.logger.error(::Spree.t(:gateway_error) + "  #{response.to_yaml}")
+          text = response.params['message'] || response.params['response_reason_text'] || response.message
+          
+          raise ::Spree::Core::GatewayError, text
+        end
 
-  response
-rescue ActiveMerchant::ConnectionError => e
-  Rails.logger.error(Spree.t(:gateway_error) + "  #{e.inspect}")
-  raise Core::GatewayError, Spree.t(:unable_to_connect_to_gateway)
-end
-
-      ::Spree::Refund.prepend SpreeRazorpayCheckout::Spree::RefundDecorator
+        response
+      rescue ActiveMerchant::ConnectionError => e
+        Rails.logger.error(::Spree.t(:gateway_error) + "  #{e.inspect}")
+        raise ::Spree::Core::GatewayError, ::Spree.t(:unable_to_connect_to_gateway)
+      end
     end
   end
 end
+
+::Spree::Refund.prepend(SpreeRazorpayCheckout::Spree::RefundDecorator) if defined?(::Spree::Refund)
