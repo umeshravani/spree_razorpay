be11e016c912436bd4f067ce25a481b597660bb8
Umesh Ravani <umeshravani98@gmail.com>
2026-04-24 02:21:49 +0530

    Add find_or_create_payment! method to Razorpay
    For Headless API V3 to properly create the source and prevent double-charges:
    
    1. Lock the order to prevent race conditions from Webhooks
    2. Check if the webhook already created this payment
    3. Create your custom RazorpayCheckout source record for Headless!
    4. Create the actual Spree::Payment

--- app/models/spree/payment_sessions/razorpay.rb
+++ app/models/spree/payment_sessions/razorpay.rb
@@ -9,6 +9,40 @@ module Spree
       def razorpay_order_id
         external_id
       end
+
+      def find_or_create_payment!(metadata = {})
+        return unless persisted?
+        return payment if payment.present?
+
+        order.with_lock do
+          rzp_payment_id = external_data['razorpay_payment_id']
+          
+          existing_payment = order.payments.where(
+            payment_method: payment_method,
+            response_code: rzp_payment_id || external_id
+          ).first
+
+          return existing_payment if existing_payment.present?
+
+          source = ::Spree::RazorpayCheckout.create!(
+            order_id: order.id,
+            razorpay_payment_id: rzp_payment_id,
+            razorpay_order_id: external_id,
+            razorpay_signature: external_data['razorpay_signature'],
+            status: 'captured',
+            payment_method: payment_method.name
+          )
+
+          order.payments.create!(
+            payment_method: payment_method,
+            amount: amount,
+            response_code: rzp_payment_id || external_id,
+            source: source,
+            skip_source_requirement: true
+          )
+        end
+      end
+
     end
   end
 end
