1e244829d11856e5c5ec86abd2e0aaabad62c295
Umesh Ravani <umeshravani98@gmail.com>
2026-07-27 18:09:38 +0530

    Add checks for table and index existence in migration

--- db/migrate/20220816080140_create_spree_razorpay_checkouts.rb
+++ db/migrate/20220816080140_create_spree_razorpay_checkouts.rb
@@ -1,23 +1,25 @@
 class CreateSpreeRazorpayCheckouts < ActiveRecord::Migration[6.1]
   def change
-    create_table :spree_razorpay_checkouts do |t|
-      t.references :order, null: true, foreign_key: { to_table: :spree_orders }
-      t.string :razorpay_order_id
-      t.string :razorpay_payment_id
-      t.string :razorpay_signature
-      t.string :status
-      t.string :payment_method
-      t.string :card_id
-      t.string :bank
-      t.string :wallet
-      t.string :vpa
-      t.string :email
-      t.string :contact
+    unless table_exists?(:spree_razorpay_checkouts)
+      create_table :spree_razorpay_checkouts do |t|
+        t.references :order, null: true, foreign_key: { to_table: :spree_orders }
+        t.string :razorpay_order_id
+        t.string :razorpay_payment_id
+        t.string :razorpay_signature
+        t.string :status
+        t.string :payment_method
+        t.string :card_id
+        t.string :bank
+        t.string :wallet
+        t.string :vpa
+        t.string :email
+        t.string :contact
 
-      t.timestamps
+        t.timestamps
+      end
     end
 
-    add_index :spree_razorpay_checkouts, :razorpay_order_id
-    add_index :spree_razorpay_checkouts, :razorpay_payment_id
+    add_index :spree_razorpay_checkouts, :razorpay_order_id unless index_exists?(:spree_razorpay_checkouts, :razorpay_order_id)
+    add_index :spree_razorpay_checkouts, :razorpay_payment_id unless index_exists?(:spree_razorpay_checkouts, :razorpay_payment_id)
   end
 end
